af8a31b2f07bbfc039e99095893db12e5210231c
yoktave-yoknel <32586011+yoktave-yoknel@users.noreply.github.com>
2020-06-24 23:47:55 +0900

    Resolves #3 名前とコメントの改善
    MinuteCount関数、HourCount関数について、
    直近60秒間あるいは60分間の累積であることを明記。
    また、クラスに説明コメントを追加

--- MinuteHourCounter.cpp
+++ MinuteHourCounter.cpp
@@ -1,3 +1,5 @@
+// 直近1分間および直近1時間の累積カウントを記録する。
+// 例えば、帯域幅の使用状況を確認するのに使える。
 class MinuteHourCounter
 {
 public:
@@ -6,9 +8,9 @@ public:
     // それから1時間は、HourCount()の返す値が+count分だけ増える。
     void Add(int count);
 
-    // 直近1分間のカウントを返す
+    // 直近60秒間の累積カウントを返す。
     int MinuteCount();
 
-    // 直近1時間のカウントを返す
+    // 直近60分の累積カウントを返す。
     int HourCount();
 }
