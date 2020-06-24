0da8da654a9bf31d13c448441debb55f31cba44a
yoktave-yoknel <32586011+yoktave-yoknel@users.noreply.github.com>
2020-06-24 23:03:13 +0900

    Resolves #3 名前とコメントの改善
    関数Count
    関数名について、動作を誤解する懸念があるためAddに変更。
    併せて引数もバイト数に固定せず汎用して、コメントも冗長なので改善。

--- MinuteHourCounter.cpp
+++ MinuteHourCounter.cpp
@@ -1,7 +1,9 @@
 class MinuteHourCounter
 {
 public:
-    // カウントを追加する
+    // 新しいデータ点を追加する (count >= 0)。
+    // それから1分間は、MinuteCount()の返す値が+count分だけ増える。
+    // それから1時間は、HourCount()の返す値が+count分だけ増える。
     void Add(int count);
 
     // 直近1分間のカウントを返す
